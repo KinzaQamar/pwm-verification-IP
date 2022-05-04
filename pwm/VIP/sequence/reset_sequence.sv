807a632857f5af67a47c80d70eb5e5b2d5a52565
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-05-04 08:53:33 +0500

    reset_sequence

--- pwm/VIP/sequence/reset_sequence.sv
+++ pwm/VIP/sequence/reset_sequence.sv
@@ -12,8 +12,8 @@
 // Language:       SystemVerilog - UVM                                                                 //
 //                                                                                                     //
 // Description:                                                                                        //
-//         reset_sequence generates rst_ni values to set or reset the DUT registers.  				  `			 //
-// Revision Date:  3rd-May-2022                                                                                      //
+//         reset_sequence generates rst_ni values to set or reset the DUT registers.  				  			 //
+// Revision Date:  3rd-May-2022                                                                        //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 class reset_sequence extends uvm_sequence # (tx_item);
 
@@ -25,21 +25,26 @@ class reset_sequence extends uvm_sequence # (tx_item);
 		super.new(name);
 	endfunction
 
+//////////////////////////////////////////Running a sequence/////////////////////////////////////////////
+
   virtual task body();
-			tx_item tx;
-			repeat(1) begin 			        								//generate transactions for block size times
-			tx = tx_item::type_id::create("tx"); 					//Body task creates transaction using factory creation
-			start_item(tx);		                  					/*start item. sequence body() blocks waiting for driver to 
-			                                       					be ready.Driver ask about sending transaction in its run phase.*/				
-																										//Wait for driver to be ready
-			if (!tx.randomize())		           						//Randomize transaction
+		tx_item tx;
+	//repeat(1) begin 			        								//generate transactions for n times
+			tx = tx_item::type_id::create("tx"); 				//Body task creates transaction using factory creation
+			start_item(tx);		                  				/*start item. sequence body() blocks waiting for driver 
+																										to be ready.Driver ask about sending transaction in 
+																										its run phase.*/				
+																									//Wait for driver to be ready
+			if (!tx.randomize())		           					//Randomize transaction
 				`uvm_fatal("Fatal","Randomization Failed")
 			tx.rst_ni  = 1'h0;
-			finish_item(tx);		          					    	/*Sends transaction and waits for response from driver to 
-												    												  know when it is ready again to generate and send 
-																										  transactions again.*/
-			end
-	endtask
+			finish_item(tx);		          					    /*Sends transaction and waits for response from driver 
+		  																							to know when it is ready again to generate and send 
+																									  transactions again.*/
+	//end
+	endtask //  virtual task body();
+
+	//////////////////////////////////////////Running a sequence/////////////////////////////////////////////
 
 	//After the body() methods returns , it passes the control back to the test.
 
