ef9830990474acd4925bda0c730319bf84de2e2a
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-05-06 01:00:41 +0500

    div_sequence

--- pwm/VIP/sequence/div_sequence.sv
+++ pwm/VIP/sequence/div_sequence.sv
@@ -16,7 +16,7 @@
 // Revision Date:  3rd-May-2022                                                                        //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-class div_sequence extends uvm_sequence # (tx_item);
+class div_sequence extends uvm_sequence # (pwm_item);
 
     //Factory Registration
 	`uvm_object_utils(div_sequence)
@@ -69,10 +69,10 @@ endclass
 //			   a response item is available from the sequencers response FIFO.
 
   task div_sequence :: body();
-		tx_item tx;
+		pwm_item tx;
 	//repeat(1) begin 			        								//generate transactions for n times
 			//Step 1 - Creation
-			tx = tx_item::type_id::create("tx"); 				//Body task creates transaction using factory creation
+			tx = pwm_item::type_id::create("tx"); 				//Body task creates transaction using factory creation
 			
 			//Step 2 - Ready - start_item()
 			start_item(tx);		                  				/*start item. sequence body() blocks waiting for driver 
