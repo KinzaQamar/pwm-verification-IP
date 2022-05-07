887ea18ce683e4d5b0a5707dedb58263b18b1962
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-05-07 21:52:57 +0500

    run.f

---
+++ pwm/VIP/compile_file/run.f
@@ -0,0 +1,36 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Company:        MICRO-ELECTRONICS RESEARCH LABORATORY                                               //
+//                                                                                                     //
+// Engineers:      Kinza Qamar Zaman - Verification                                                    //
+//                                                                                                     //
+// Additional contributions by:                                                                        //
+//                                                                                                     //
+// Create Date:    20-MARCH-2022                                                                       //
+// Design Name:    Analysis port example                                                               //
+// Module Name:    run.f                                                                               //
+// Project Name:   Monitor and Agent analysis port example   		                                       //
+// Language:       SystemVerilog - UVM                                                                 //
+//                                                                                                     //
+// Description:                                                                                        //
+//         -  This file contain names of the files to be compiled, elaborate and simulate.             //
+// Revision Date:                                                                                      //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+-64
+
+-uvmhome $UVMHOME
+
+//For simulating on Xcelium SimVision GUI mode, uncomment the below line
+//-access +rwc -gui
+
+//TEST_NAME
++UVM_TESTNAME=pwm_test 
+
+//Seed randomization
+-svseed random
+
+// compile files
+../pkg/base_class_pkg.sv
+../top/top.sv          
+
+
