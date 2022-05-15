09528e9b8d7ef8a0948827e238e140b2cdf2ac4a
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-05-15 13:05:25 +0500

    pwm_interface

--- pwm/VIP/interface/pwm_interface.sv
+++ pwm/VIP/interface/pwm_interface.sv
@@ -13,7 +13,7 @@
 //                                                                                                     //
 // Description:                                                                                        //
 //             PWM interface has methods and signals to drive pwm_items to DUT.                        //
-// Revision Date:  9-MAY-2022                                                                          //
+// Revision Date:  15-MAY-2022                                                                         //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 interface pwm_interface;
@@ -65,6 +65,18 @@ interface pwm_interface;
     o_pwm_2 = tx.o_pwm_2;
     oe_pwm1 = tx.oe_pwm1;
     oe_pwm2 = tx.oe_pwm2;
+    $display("//////////////////////////////////////////INTERFACE transaction METHODS//////////////////////");
+    $display(" rst_ni  = 0x%0h",rst_ni );    
+    $display(" write   = 0x%0h",write  );
+    $display(" addr_i  = 0x%0h",addr_i );
+    $display(" wdata_i = 0x%0h",wdata_i);
+    $display(" rdata_o = 0x%0h",rdata_o);
+    $display(" o_pwm   = 0x%0h",o_pwm  );
+    $display(" o_pwm_2 = 0x%0h",o_pwm_2);
+    $display(" oe_pwm1 = 0x%0h",oe_pwm1);
+    $display(" oe_pwm2 = 0x%0h",oe_pwm2);    
+    //$display("rst_ni = 0x%0h \nwrite = %0d \naddr_i = %0d \nwdata_i = %0h \nrdata_o = %0d \no_pwm = %0d \no_pwm_2 = %0d \noe_pwm1 = %0d \noe_pwm2 = %0d" , rst_ni, write, addr_i, wdata_i, rdata_o, o_pwm, o_pwm_2, oe_pwm1, oe_pwm2);
+    $display("//////////////////////////////////////////INTERFACE transaction METHODS//////////////////////");
   endtask // task automatic transaction (pwm_item tx);
 
   task automatic clk_gen (input bit clk);
@@ -86,10 +98,21 @@ interface pwm_interface;
     tx.oe_pwm2 = oe_pwm2 ;
 	endtask // task automatic get_an_output (pwm_item tx);
 
-  task automatic print_interface_transaction ();
-		get_an_input  (tx);
+  task automatic print_interface_transaction (pwm_item tx);
+    get_an_input  (tx);
 		get_an_output (tx);
-		`uvm_info("INTERFACE SIGNALS",tx.convert2string,UVM_DEBUG);
+    $display("//////////////////////////////////////////INTERFACE print_interface_transaction METHOD//////////////////////");
+    $display(" tx.rst_ni  = 0x%0h",tx.rst_ni );    
+    $display(" tx.write   = 0x%0h",tx.write  );
+    $display(" tx.addr_i  = 0x%0h",tx.addr_i );
+    $display(" tx.wdata_i = 0x%0h",tx.wdata_i);
+    $display(" tx.rdata_o = 0x%0h",tx.rdata_o);
+    $display(" tx.o_pwm   = 0x%0h",tx.o_pwm  );
+    $display(" tx.o_pwm_2 = 0x%0h",tx.o_pwm_2);
+    $display(" tx.oe_pwm1 = 0x%0h",tx.oe_pwm1);
+    $display(" tx.oe_pwm2 = 0x%0h",tx.oe_pwm2);
+    //$display("tx.rst_ni = %0d \ntx.write = %0d \ntx.addr_i = %0d \ntx.wdata_i = %0h \ntx.rdata_o = %0d \ntx.o_pwm = %0d \ntx.o_pwm_2 = %0d \ntx.oe_pwm1 = %0d \ntx.oe_pwm2 = %0d" , tx.rst_ni, tx.write, tx.addr_i, tx.wdata_i, tx.rdata_o, tx.o_pwm, tx.o_pwm_2, tx.oe_pwm1, tx.oe_pwm2);
+    $display("//////////////////////////////////////////INTERFACE print_interface_transaction METHOD//////////////////////");
 	endtask // task automatic print_interface_transaction (pwm_item tx);
 
 endinterface
