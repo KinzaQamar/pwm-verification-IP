789c49bdc00fe3d486114fe0f41bd9b1db50045f
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-05-24 18:04:36 +0500

    Added virtual sequence

--- pwm/VIP/virtual_sequence/pwm_vseq.sv
+++ pwm/VIP/virtual_sequence/pwm_vseq.sv
@@ -5,7 +5,7 @@
 //                                                                                                     //
 // Additional contributions by:                                                                        //
 //                                                                                                     //
-// Create Date:    23-MAY-2022                                                                         //
+// Create Date:    23rd-MAY-2022                                                                       //
 // Design Name:    PWM Verification IP                                                                 //
 // Module Name:    pwm_vseq.sv                                                                         //
 // Project Name:   PWM Verification IP.                                                                //
@@ -13,19 +13,42 @@
 //                                                                                                     //
 // Description:                                                                                        //
 //          	 pwm_vseq is a sequence that creates and start other sequences.         								 //
-// Revision Date:                                                                                      //
+// Revision Date:  24th-MAY-2022                                                                       //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class pwm_vseq extends uvm_sequence;
 
-	uvm_sequencer #(pwm_item) pwm_sqr;
+  //Factory Registration
+	`uvm_object_utils(pwm_vseq)
 
+	//Constructor
+	function new(string name="pwm_vseq");
+		super.new(name);
+	endfunction
+	
+	//declare the sequence handle
+	uvm_sequencer #(pwm_item) pwm_sqr;
 	div_sequence div_seq;
 	period_sequence period_seq;
 	dc_sequence dc_seq;
 	ctrl_sequence ctrl_seq;
 
-	task body();
+//////////////////////////////////////////METHODS///////////////////////////////////////////////////////
+
+	extern virtual task init_start_seq (output uvm_status_e txn_status,
+															 				input uvm_sequencer #(pwm_item) pwm_sqr_i
+															 				//other sequencer handles
+															 				);
+
+	extern virtual task body();
+
+endclass
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//-----------------------------------body Method------------------------------------------------------//
+
+	task pwm_vseq :: body();
+
 		div_seq = div_sequence::type_id::create("div_seq");
 		period_seq = period_sequence::type_id::create("period_seq");
 		dc_seq = dc_sequence::type_id::create("dc_seq");
@@ -37,7 +60,23 @@ class pwm_vseq extends uvm_sequence;
 			dc_seq.start(pwm_sqr,this);
 			ctrl_seq.start(pwm_sqr,this);			
 		end
-		
+
 	endtask
 
-endclass
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//-----------------------------------body Method------------------------------------------------------//
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//-----------------------------------init_start_seq Method--------------------------------------------//
+
+	task pwm_vseq :: init_start_seq (output uvm_status_e txn_status,
+																	 input uvm_sequencer #(pwm_item) pwm_sqr_i
+																	 //other sequencer handles
+																	);
+		pwm_sqr = pwm_sqr_i;
+		this.start(null);
+
+	endtask
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//-----------------------------------init_start_seq Method--------------------------------------------//
